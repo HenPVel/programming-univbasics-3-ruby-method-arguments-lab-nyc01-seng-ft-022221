fc8869151c61321b8c3aa8879bebab19fabd27bb
Henry Velasquez <hvelas93@gmail.com>
2021-02-04 16:16:40 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,11 +4,28 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+# Define a method called `introduction` that takes in the parameter `name` and outputs
+# the phrase: `"Hi, my name is #{name}."`
 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
+#Define a method called `introduction_with_language` that takes in two parameters,
+# `name` and `language` and outputs the phrase: `"Hi, my name is #{name} and I am
+#learning to program in #{language}."`
 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+#Copy the previous method and name it `introduction_with_language_optional`. It
+#takes in two parameters, `name` and `language`, but the second parameter is
+#optional. The default value for `language` will be "Ruby".
 
+def introduction_with_language_optional(name,language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
